fa82ce30cb69f3defed23a17dad943a4efc38e9c
RedoTV <redotv1@gmail.com>
2024-02-04 21:23:28 +0300

    made header, login and signUp pages (markdown)

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { slideInAnimation } from './animation';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent],
+    animations:[slideInAnimation]
 })
 export class AppComponent {
   title = 'Client';
+  constructor(private contexts: ChildrenOutletContexts) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
--- src/Client/src/app/app.routes.ts
+++ src/Client/src/app/app.routes.ts
@@ -1,15 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     {
         path: "",
         pathMatch: "full",
-        component: HomeComponent
+        component: HomeComponent,
+        data: {
+            animation:"slideInAnimation"
+        }
     },
     {
         path:"login",
-        component: LogInComponent
+        component: LogInComponent,
+        data: {
+            animation:"slideInAnimation"
+        }
+    },
+    {
+        path:"signup",
+        component:SignUpComponent,
+        data: {
+            animation:"slideInAnimation"
+        }
     }
 ];
